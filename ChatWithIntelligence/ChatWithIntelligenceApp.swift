93cc63a8a622c34e8babcf2c2a3d2431d5e3b19d
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2025-06-13 19:29:21 +0900

    Initial Commit

---
+++ ChatWithIntelligence/ChatWithIntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithIntelligenceApp.swift
+//  ChatWithIntelligence
+//
+//  Created by Venti on 13/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithIntelligence/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChatWithIntelligence
+//
+//  Created by Venti on 13/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
